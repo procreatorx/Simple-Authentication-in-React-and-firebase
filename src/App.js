e6b926802958a8b383a39d4320b5cc27b42dc2ab
procreatorx <44638829+procreatorx@users.noreply.github.com>
2018-10-31 16:43:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+// import firebase from 'firebase';
+import { Header, Button, Spinner, CardSection, } from './components/common';
+import LoginForm from './components/LoginForm';
+
+
+class App extends Component {
+        constructor(props) {
+            super(props);
+           this.state = { loggedIn: null };         
+        }
+   
+     componentWillMount() {
+     this.initializeFirebase(); 
+    }
+
+    initializeFirebase() {
+        const firebase = require('firebase');
+      
+     const config = {
+            your Apikey
+          };
+          if (!firebase.apps.length) {
+            firebase.initializeApp(config);
+         }
+        
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+}
+      renderContent() {
+        const firebase = require('firebase');
+
+        switch (this.state.loggedIn) {
+            case true:
+            return (
+                <CardSection>
+                 <Button onPress={() => firebase.auth().signOut()}>
+                 Log Out
+                </Button> 
+                </CardSection>
+            );
+            case false:
+            return <LoginForm />;
+            default:
+            return (
+                <CardSection>
+                <Spinner size='large' />
+                </CardSection>
+        );
+        } 
+}
+
+    render() {
+        return (
+            <View>
+            <Header headerText={'Auth'} />
+             {this.renderContent()}
+            </View>
+        );
+    }
+}
+
+export default App;
